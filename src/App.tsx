d5181d7f24b225d1b3002ffd9e71989e8e625510
khaled.chehab <khchehab@gmail.com>
2022-08-27 20:59:00 +0300

    refactored code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import './index.css';
 import NavigationBar from './components/UI/NavigationBar';
-import { Outlet } from 'react-router-dom';
+import { CharacterProvider } from './contexts/character-context';
+import Characters from './pages/Characters';
+import Episodes from './pages/Episodes';
+import Locations from './pages/Locations';
+import PageNotFound from './pages/PageNotFound';
 
-export default function App() {
+function RickAndMortyApp() {
     return (<>
         <NavigationBar />
         <Outlet />
     </>);
+}
+
+export default function App() {
+    return (<React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<RickAndMortyApp />}>
+                    <Route index element={<CharacterProvider>
+                        <Characters />
+                    </CharacterProvider>} />
+                    <Route path='episodes' element={<Episodes />} />
+                    <Route path='locations' element={<Locations />} />
+                    <Route path='*' element={<PageNotFound />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,5 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.css';
 import App from './App';
-import { CharacterProvider } from './contexts/character-context';
-import Characters from './pages/Characters';
-import Episodes from './pages/Episodes';
-import Locations from './pages/Locations';
-import PageNotFound from './pages/PageNotFound';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(<React.StrictMode>
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<App />}>
-                <Route index element={<CharacterProvider>
-                    <Characters />
-                </CharacterProvider>} />
-                <Route path='episodes' element={<Episodes />} />
-                <Route path='locations' element={<Locations />} />
-                <Route path='*' element={<PageNotFound />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>
-</React.StrictMode>);
+root.render(<App />);
