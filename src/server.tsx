05125e48dfc9f4e4c5ec32a8a5c4f2f0fdb8bed9
khaled.chehab <khchehab@gmail.com>
2022-08-30 00:10:57 +0300

    Added favicon to the express server

--- src/server.tsx
+++ src/server.tsx
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import express from 'express';
+import favicon from 'serve-favicon';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import App from './App';
@@ -8,6 +9,7 @@ import App from './App';
 const app = express();
 
 app.use(express.static('dist'));
+app.use(favicon(path.resolve(__dirname, '..', 'public', 'favicon.ico')));
 app.get('/', (req, res) => {
     const content = renderToString(
         <StaticRouter location={req.path}>
@@ -22,3 +24,4 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => console.log('Server is listening on port 3000!'));
+
